444dc7eabf811002f569d97913f754d1afb49d9f
Jasper Zhou <jzj.pers@gmail.com>
2023-04-08 12:37:30 +0800

    Initial commit. Splashscreen created and working on Register/Login screen.

---
+++ app/src/main/java/com/jasperzj/studibuds/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.jasperzj.studibuds
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginOrRegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1750)
+    }
+}
